65f6c17643cf22512bcca9b17eae9466d462a25b
hofmannicusrex <66104428+hofmannicusrex@users.noreply.github.com>
2020-06-02 22:04:33 -0500

    Add files via upload

---
+++ VariableAssignmentHofmann.py
@@ -0,0 +1,12 @@
+#    VariableAssignmentHofmann || Nick Hofmann 6/2/2020
+#    nickhofmann1989@hotmail.com | nohofmann@dmacc.edu
+#
+#    This program will print out 3 variables that I will declare.
+
+quantity = 1337
+item = "knee board"
+size = .9
+
+print("quantity =", quantity, "\nitem =", item, "\nsize =", size)
+
+input("\nPress enter to exit.")
