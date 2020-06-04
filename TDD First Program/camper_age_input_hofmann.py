19ccc95e30c9d4ed0645da10e88388902fd58609
hofmannicusrex <66104428+hofmannicusrex@users.noreply.github.com>
2020-06-03 23:19:01 -0500

    Add files via upload

---
+++ TDD First Program/camper_age_input_hofmann.py
@@ -0,0 +1,16 @@
+"""
+Program: camper_age_input_hofmann.py
+Author: Nick Hofmann | nickhofmann1989@hotmail.com | nohofmann@dmacc.edu
+Last Modified: 6/3/2020
+
+In this program I will create a function for printing 'Hello World!'.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+# End of camper_age_input_hofmann.py
