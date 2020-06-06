adc3133d429ac47fd11c70e359db07d5b27b4b9a
hofmannicusrex <nickhofmann1989@hotmail.com>
2020-06-06 17:07:28 -0500

    Testing for Github upload.

--- Module3/strings_and_string_methods.py
+++ Module3/strings_and_string_methods.py
@@ -1,1 +1,8 @@
+"""
+Program: strings_and_string_methods.py
+Author: Nick Hofmann | nickhofmann1989@hotmail.com | nohofmann@dmacc.edu
+Last Modified: 6/6/2020
 
+Program specifications: The program will have will prompt for the age of one infant (age 1-5 years)
+who is attending camp and convert the age in months to years via a function call then print the result.
+"""
