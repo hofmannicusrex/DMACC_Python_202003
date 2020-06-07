50267cfa7d9887f9fd030e1147f905ee34727abd
hofmannicusrex <nickhofmann1989@hotmail.com>
2020-06-06 23:15:12 -0500

    Test passing push

--- Module3/format_output/average_scores.py
+++ Module3/format_output/average_scores.py
@@ -5,3 +5,24 @@ Last Modified: 6/6/2020
 
 Program specifications: The program will.
 """
+
+# METHODS
+
+
+def average():
+    # Get input for scores
+    test_score_1 = input('Please enter the test score: ')
+    test_score_2 = input('Please enter the test score: ')
+    test_score_3 = input('Please enter the test score: ')
+
+    # return calculate_average(test_score_1, test_score_2, test_score_3)  # Alternative way return statement.
+    return (test_score_1 + test_score_2 + test_score_3) / 3  # calculation using score1, score2 and score3
+
+# Alternative way presented by the professor.
+# I wrote out this code because it was a great refresher for me as I took last semester off and I'm
+# not a coding expert yet.
+
+
+def calculate_average(score1, score2, score3):
+    score_average = ((score1 + score2 + score3) / 3)  # I'm a fan of parentheses.
+    return score_average
--- Module3/test/test_average_scores.py
+++ Module3/test/test_average_scores.py
@@ -5,3 +5,16 @@ Last Modified: 6/6/2020
 
 Program specifications: The program will .
 """
+import  unittest
+import unittest.mock as mock
+from Module3.format_output import average_scores as avg
+
+
+class MyTestCase(unittest.TestCase):
+    def test_average(self):
+        with mock.patch('builtins.input', side_effect=[85, 90, 95]):
+            assert avg.average() == 90
+
+
+if __name__ == '__main__':
+    unittest.main()
