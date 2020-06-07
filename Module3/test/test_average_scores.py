c0b03f99e73a15f8cd987863ff075bd33e5d5a29
hofmannicusrex <nickhofmann1989@hotmail.com>
2020-06-06 22:35:59 -0500

    Initial push

---
+++ Module3/test/test_average_scores.py
@@ -0,0 +1,7 @@
+"""
+Program: test_average_scores.py
+Author: Nick Hofmann | nickhofmann1989@hotmail.com | nohofmann@dmacc.edu
+Last Modified: 6/6/2020
+
+Program specifications: The program will .
+"""
