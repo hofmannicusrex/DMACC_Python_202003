f34a9b94832cb65c907b0fae3e55f9e0d1962fac
hofmannicusrex <nickhofmann1989@hotmail.com>
2020-06-06 23:58:08 -0500

    Final, working code for average_scores.py

--- Module3/format_output/average_scores.py
+++ Module3/format_output/average_scores.py
@@ -11,9 +11,9 @@ Program specifications: The program will.
 
 def average():
     # Get input for scores
-    test_score_1 = input('Please enter the test score: ')
-    test_score_2 = input('Please enter the test score: ')
-    test_score_3 = input('Please enter the test score: ')
+    test_score_1 = int(input('Please enter the test score: '))
+    test_score_2 = int(input('Please enter the test score: '))
+    test_score_3 = int(input('Please enter the test score: '))
 
     # return calculate_average(test_score_1, test_score_2, test_score_3)  # Alternative way return statement.
     return (test_score_1 + test_score_2 + test_score_3) / 3  # calculation using score1, score2 and score3
@@ -26,3 +26,11 @@ def average():
 def calculate_average(score1, score2, score3):
     score_average = ((score1 + score2 + score3) / 3)  # I'm a fan of parentheses.
     return score_average
+
+
+if __name__ == '__main__':
+    first_name = input('Enter the test taker\'s first name: ')
+    last_name = input('Enter the test taker\'s last name: ')
+    age = input('Enter the test taker\'s age: ')
+    average_scores = average()
+    print('\n', last_name, ', ', first_name, ' age: ', age, ' average test score: %4.2f' % average_scores, '.', sep='')
