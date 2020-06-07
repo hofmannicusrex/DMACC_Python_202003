b0847ca75acde671d783cdb1a2fab2bcddabffa3
hofmannicusrex <66104428+hofmannicusrex@users.noreply.github.com>
2020-06-06 19:52:48 -0500

    Create lists_and_list_methods.py

---
+++ Module3/lists_and_list_methods.py
@@ -0,0 +1,7 @@
+"""
+Program: lists_and_list_methods.py
+Author: Nick Hofmann | nickhofmann1989@hotmail.com | nohofmann@dmacc.edu
+Last Modified: 6/6/2020
+
+Program specifications: The program will demonstrate different Python list methods.
+"""
