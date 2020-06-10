1ffc66f5f4f37fa76acfbd83fedd12dcdeca0c01
hofmannicusrex <nickhofmann1989@hotmail.com>
2020-06-10 15:31:38 -0500

    Initial push to Git

---
+++ Module4/nested_if_statements_hofmann/test/test_coupon_calculations.py
@@ -0,0 +1,7 @@
+"""
+Program: test_coupon_calculations.py
+Author: Nick Hofmann | nickhofmann1989@hotmail.com | nohofmann@dmacc.edu
+Last Modified: 6/10/2020
+
+Program specifications: The program will .
+"""
