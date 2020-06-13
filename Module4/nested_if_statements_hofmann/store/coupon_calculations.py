b015959df1c7f68ba340423b4cc60fac00712d08
hofmannicusrex <nickhofmann1989@hotmail.com>
2020-06-12 23:01:11 -0500

    Final (incorrect solution) to Git. Can't get testing function to work since my formatted output becomes a type of 'none' or null and you can't compare a null to a float...

--- Module4/nested_if_statements_hofmann/store/coupon_calculations.py
+++ Module4/nested_if_statements_hofmann/store/coupon_calculations.py
@@ -1,7 +1,94 @@
 """
 Program: coupon_calculations.py
 Author: Nick Hofmann | nickhofmann1989@hotmail.com | nohofmann@dmacc.edu
-Last Modified: 6/10/2020
+Last Modified: 6/12/2020
 
-Program specifications: The program will .
+Program specifications: The program will calculate the total cost of the order based off of
+three user inputs: sticker price of the item, value of their cash coupon, and amount of their
+percent coupon.
 """
+import constants as const
+
+
+def calculate_price(price, cash_coupon, percent_coupon):
+
+    price_after_cash_coupon = price - cash_coupon
+
+    # Do I have a cash coupon? How does that affect the outcome?
+    # Do I have a discount? HOw does that affect the outcome?
+    # What is the shipping cost? How can I calculate that/add it in?
+    # Using the outer if statement to determine the shipping cost since it's based off of the init. price - cash_coupon
+    if 0 <= price_after_cash_coupon < const.TEN_DOLLAR_THRESHOLD:  # if 0 <= price_after_cash_coupon < 10
+
+        if percent_coupon == 10:  # AND percent_coupon is 10%
+            final_price = ((price_after_cash_coupon * const.PERCENT_COUPON_10) + const.UNDER_10_SHIPPING) * const.SALES_TAX_RATE
+            print('\nThe final order price is: ', '${:,.2f}'.format(final_price))
+
+        elif percent_coupon == 15:  # AND percent_coupon is 15%
+            final_price = ((price_after_cash_coupon * const.PERCENT_COUPON_15) + const.UNDER_10_SHIPPING) * const.SALES_TAX_RATE
+            print('\nThe final order price is: ', '${:,.2f}'.format(final_price))
+
+        elif percent_coupon == 20:  # AND percent_coupon is 20%
+            final_price = ((price_after_cash_coupon * const.PERCENT_COUPON_20) + const.UNDER_10_SHIPPING) * const.SALES_TAX_RATE
+            print('\nThe final order price is: ', '${:,.2f}'.format(final_price))
+
+    elif const.TEN_DOLLAR_THRESHOLD <= price_after_cash_coupon < const.THIRTY_DOLLAR_THRESHOLD:  # if 10 <= price_after_cash_coupon < 30
+
+        if percent_coupon == 10:  # AND percent_coupon is 10%
+            final_price = ((price_after_cash_coupon * const.PERCENT_COUPON_10) + const.BETWEEN_10_TO_30_SHIPPING) * const.SALES_TAX_RATE
+            print('\nThe final order price is: ', '${:,.2f}'.format(final_price))
+
+        elif percent_coupon == 15:  # AND percent_coupon is 15%
+            final_price = ((price_after_cash_coupon * const.PERCENT_COUPON_15) + const.BETWEEN_10_TO_30_SHIPPING) * const.SALES_TAX_RATE
+            print('\nThe final order price is: ', '${:,.2f}'.format(final_price))
+
+        elif percent_coupon == 20:  # AND percent_coupon is 20%
+            final_price = ((price_after_cash_coupon * const.PERCENT_COUPON_20) + const.BETWEEN_10_TO_30_SHIPPING) * const.SALES_TAX_RATE
+            print('\nThe final order price is: ', '${:,.2f}'.format(final_price))
+
+    elif const.THIRTY_DOLLAR_THRESHOLD <= price_after_cash_coupon < const.FIFTY_DOLLAR_THRESHOLD:  # if 30 <= price_after_cash_coupon < 50
+
+        if percent_coupon == 10:  # AND percent_coupon is 10%
+            final_price = ((price_after_cash_coupon * const.PERCENT_COUPON_10) + const.BETWEEN_30_TO_50_SHIPPING) * const.SALES_TAX_RATE
+            print('\nThe final order price is: ', '${:,.2f}'.format(final_price))
+
+        elif percent_coupon == 15:  # AND percent_coupon is 15%
+            final_price = ((price_after_cash_coupon * const.PERCENT_COUPON_15) + const.BETWEEN_30_TO_50_SHIPPING) * const.SALES_TAX_RATE
+            print('\nThe final order price is: ', '${:,.2f}'.format(final_price))
+
+        elif percent_coupon == 20:  # AND percent_coupon is 20%
+            final_price = ((price_after_cash_coupon * const.PERCENT_COUPON_20) + const.BETWEEN_30_TO_50_SHIPPING) * const.SALES_TAX_RATE
+            print('\nThe final order price is: ', '${:,.2f}'.format(final_price))
+
+    elif price_after_cash_coupon > const.FIFTY_DOLLAR_THRESHOLD:  # if price_after_cash_coupon > 50
+
+        if percent_coupon == 10:  # AND percent_coupon is 10%
+            final_price = ((price_after_cash_coupon * const.PERCENT_COUPON_10) + const.OVER_50_SHIPPING) * const.SALES_TAX_RATE
+            print('\nThe final order price is: ', '${:,.2f}'.format(final_price))
+
+        elif percent_coupon == 15:  # AND percent_coupon is 15%
+            final_price = ((price_after_cash_coupon * const.PERCENT_COUPON_15) + const.OVER_50_SHIPPING) * const.SALES_TAX_RATE
+            print('\nThe final order price is: ', '${:,.2f}'.format(final_price))
+
+        elif percent_coupon == 20:  # AND percent_coupon is 20%
+            final_price = ((price_after_cash_coupon * const.PERCENT_COUPON_20) + const.OVER_50_SHIPPING) * const.SALES_TAX_RATE
+            print('\nThe final order price is: ', '${:,.2f}'.format(final_price))
+
+    else:
+        print('\nWe\'re sorry but your total order price cannot be a negative dollar amount. Please try again.')
+
+    # Outer if - elif statement for price variable ranges
+    #       Inner if - elif statements in each of those 4 price ranges?
+
+    # IF STATEMENT MAY ONLY BE 12 OR 13 LINES ACCORDING TO PROFESSOR
+
+    # Subtotal, now add tax.
+    # return your total value
+
+
+if __name__ == '__main__':
+    price = float(input('What is the sticker or initial price of the item? '))
+    cash_coupon = float(int(input('What kind of cash coupon do you have? ($5 or $10) ')))
+    percent_coupon = float(int(input('What is the amount of your percent coupon? (10%, 15%, 20%) ')))
+
+    calculate_price(price, cash_coupon, percent_coupon)
--- Module4/nested_if_statements_hofmann/test/test_coupon_calculations.py
+++ Module4/nested_if_statements_hofmann/test/test_coupon_calculations.py
@@ -1,7 +1,56 @@
 """
 Program: test_coupon_calculations.py
 Author: Nick Hofmann | nickhofmann1989@hotmail.com | nohofmann@dmacc.edu
-Last Modified: 6/10/2020
+Last Modified: 6/12/2020
 
-Program specifications: The program will .
+Program specifications: The program will test coupon_calculations.py.
 """
+import unittest
+import unittest.mock as mock
+from store import coupon_calculations as coupon_calc
+
+
+class MyTestCase(unittest.TestCase):
+    # def calculate_price(price, cash_coupon, percent_coupon):
+    def test_price_under_ten(self):  # Test at the low and high sides of the range
+        price_under_10_1 = 7.99
+        price_under_10_2 = 1.12
+        # Tests for the value of price_under_10_1
+        self.assertAlmostEqual(coupon_calc.calculate_price(price_under_10_1, 5, 10), 9.16, places=2)
+        self.assertAlmostEqual(coupon_calc.calculate_price(price_under_10_1, 5, 15), 9, places=2)
+        self.assertAlmostEqual(coupon_calc.calculate_price(price_under_10_1, 5, 20), 8.84, places=2)
+        self.assertAlmostEqual(coupon_calc.calculate_price(price_under_10_1, 10, 10), 4.39, places=2)
+        self.assertAlmostEqual(coupon_calc.calculate_price(price_under_10_1, 10, 15), 4.5, places=2)
+        self.assertAlmostEqual(coupon_calc.calculate_price(price_under_10_1, 10, 20), 4.6, places=2)
+        # Tests for the value of price_under_10_2
+        self.assertAlmostEqual(coupon_calc.calculate_price(price_under_10_2, 5, 10), 2.61, places=2)
+        self.assertAlmostEqual(coupon_calc.calculate_price(price_under_10_2, 5, 15), 2.81, places=2)
+        self.assertAlmostEqual(coupon_calc.calculate_price(price_under_10_2, 5, 20), 3.02, places=2)
+        self.assertAlmostEqual(coupon_calc.calculate_price(price_under_10_2, 10, 10), -2.16, places=2)
+        self.assertAlmostEqual(coupon_calc.calculate_price(price_under_10_2, 10, 15), -1.69, places=2)
+        self.assertAlmostEqual(coupon_calc.calculate_price(price_under_10_2, 10, 20), -1.22, places=2)
+
+        #     def test_price_ten_to_thirty(self):
+        #         price_between_10_and_30 = 6.90
+        #         self.assertAlmostEqual(coupon_calc.calculate_price(price_between_10_and_30, 5, 10), 9.48, places=4)
+        #         self.assertAlmostEqual(coupon_calc.calculate_price(price_between_10_and_30, 5, 15), 9.48, places=4)
+        #         self.assertAlmostEqual(coupon_calc.calculate_price(price_between_10_and_30, 5, 20), 9.48, places=4)
+        #         self.assertAlmostEqual(coupon_calc.calculate_price(price_between_10_and_30, 10, 10), 9.48, places=4)
+        #
+        #     def test_price_thirty_to_fifty(self):
+        #         price_between_30_and_50 = 6.90
+        #         self.assertAlmostEqual(coupon_calc.calculate_price(price_between_30_and_50, 5, 10), 9.48, places=4)
+        #         self.assertAlmostEqual(coupon_calc.calculate_price(price_between_30_and_50, 5, 15), 9.48, places=4)
+        #         self.assertAlmostEqual(coupon_calc.calculate_price(price_between_30_and_50, 5, 20), 9.48, places=4)
+        #         self.assertAlmostEqual(coupon_calc.calculate_price(price_between_30_and_50, 10, 10), 9.48, places=4)
+        #
+        #     def test_price_over_fifty(self):
+        #         price_over_50 = 566.90
+        #         self.assertAlmostEqual(coupon_calc.calculate_price(price_over_50, 5, 10), 9.48, places=4)
+        #         self.assertAlmostEqual(coupon_calc.calculate_price(price_over_50, 5, 15), 9.48, places=4)
+        #         self.assertAlmostEqual(coupon_calc.calculate_price(price_over_50, 5, 20), 9.48, places=4)
+        #         self.assertAlmostEqual(coupon_calc.calculate_price(price_over_50, 10, 10), 9.48, places=4)
+
+
+if __name__ == '__main__':
+    unittest.main()
