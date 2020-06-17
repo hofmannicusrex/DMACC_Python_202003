3ee225ec26f668d18f8df4b2e108b6e51ab6d745
hofmannicusrex <nickhofmann1989@hotmail.com>
2020-06-16 22:06:29 -0500

    Final push to Git

---
+++ Module6/function_parameter_assignment/function_parameter_assignment_main/function_parameter_assignment_hofmann.py
@@ -0,0 +1,20 @@
+"""
+Program: function_parameter_assignment_hofmann.py
+Author: Nick Hofmann | nickhofmann1989@hotmail.com | nohofmann@dmacc.edu
+Last Modified: 6/16/2020
+
+Program specifications: The program will .
+"""
+
+
+def multiply_string(message, n):
+    """
+    :param message: String containing my favorite class this semester.
+    :param n: Int containing the number of times I want the message to print.
+    :return: The string * the int.
+    """
+    return print(message * n)
+
+
+if __name__ == '__main__':
+    multiply_string('C++\t', 6)
