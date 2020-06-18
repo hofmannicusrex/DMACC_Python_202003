b8a5b3578c21d0a88aedd08eed60e1d9a170edb1
hofmannicusrex <nickhofmann1989@hotmail.com>
2020-06-17 23:24:34 -0500

    First unittest passing push

--- Module6/functions_default_values_assignment/more_functions/validate_input_in_functions.py
+++ Module6/functions_default_values_assignment/more_functions/validate_input_in_functions.py
@@ -3,56 +3,31 @@ Program: validate_input_in_functions.py
 Author: Nick Hofmann | nickhofmann1989@hotmail.com | nohofmann@dmacc.edu
 Last Modified: 6/17/2020
 
-Program specifications: This program will repeatedly ask the user .
+Program specifications: This program will repeatedly ask the user for a valid test
+score until it is range (0-100). It will then display the test taker's name and score.
 """
 
-"""
-def ask_ok(prompt, retries=4, reminder='Please try again!'):
-    while True:
-        ok = input(prompt)
-        if ok in ('y', 'ye', 'yes'):
-            return True
-        if ok in ('n', 'no', 'nop', 'nope'):
-            return False
-        retries = retries - 1
-        if retries < 0:
-            raise ValueError('invalid user response')
-        print(reminder)
 
-
-if __name__ == '__main__':
-    ask_ok('Do you really want to quit?')
-"""
-
-"""     MY CODE MY CODE MY CODE MY CODE MY CODE MY CODE MY CODE MY CODE MY CODE
-def score_input(test_name, test_score=0, invalid_message='Invalid test score, try again!'):
-    while True:
-        test_score_entry = int(input(test_name))
-        if 0 <= test_score_entry <= 100:
-            test_score_string = 'Test name: ' + str(test_score_entry)
-            print(test_score_string)
-            return
-        print('You entered:', test_score_entry, '\nThat is not in range: please try again. :)\n')
-        MY CODE MY CODE MY CODE MY CODE MY CODE MY CODE MY CODE MY CODE MY CODE """
-
-
-def score_input(test_name, test_score=0, invalid_message='Invalid test score, try again!'):
+def score_input(test_name, test_score=0, invalid_message='Invalid test score, try again! '):
     """
-    :param test_name: The name of the test...not really sure what a "test name" is but the
-                      assignment requires it.
+    :param test_name: The name of the test taker.
     :param test_score: The score that was earned on the test.
-    :param invalid_message: The message that will be displayed upon the user entering a bad
-                            input.
+    :param invalid_message: The message that will be displayed upon the user entering bad input.
     :return: Returns valid input in this format: "test_name: test_score"
     """
-    # return {test_name: test_score}
-    return
+
+    while True:
+
+        if test_score < 0 or test_score > 100:
+            test_score = int(input(invalid_message))
+
+        else:
+            final_output = '\n' + test_name + ': ' + str(test_score) + '%'
+            return final_output
     pass
 
 
 if __name__ == '__main__':
-    # score_input('Please enter a test score between 0 and 100, please and spank you: ')
     user_test_name = input('Please enter your name: ')
-    user_test_score = input('Please enter a test score between 0 and 100, please and spank you: ')
-    # int-casting user_test_score might not be necessary.
-    score_input(user_test_name, int(user_test_score))  # Consider wrapping this in a print statement.
+    user_test_score = int(input('Please enter a test score between 0 and 100, please and spank you: '))
+    print(score_input(user_test_name, user_test_score))
--- Module6/functions_default_values_assignment/test_functions/test_valid_input_in_functions.py
+++ Module6/functions_default_values_assignment/test_functions/test_valid_input_in_functions.py
@@ -11,5 +11,21 @@ import more_functions.validate_input_in_functions as test_score_input
 
 class MyTestCase(unittest.TestCase):
 
-    def test_score_input(self):
-        self.assertEqual('Nick Hofmann: 97', test_score_input.score_input('Nick Hofmann', 97))
+    def test_score_input_test_name(self):
+        self.assertEqual('\nNick Hofmann: 0%', test_score_input.score_input('Nick Hofmann'))
+
+    # def test_score_input_test_score_valid(self):
+        # self.assertEqual('\nNick Hofmann: 97%', test_score_input.score_input('Nick Hofmann', 97))
+
+    # def test_score_input_test_score_below_range(self):
+        # self.assertEqual('Invalid test score, try again! ', test_score_input.score_input('Nick Hofmann', -1))
+
+    # def test_score_input_test_score_above_range(self):
+        # self.assertEqual('\nNick Hofmann: 97%', test_score_input.score_input('Nick Hofmann', 666))
+
+    # def test_score_input_test_score_non_numeric(self):
+        # self.assertEqual('\nNick Hofmann: 97%', test_score_input.score_input('Nick Hofmann', 'non numeric'))
+        # pass
+
+    # def test_score_input_test_invalid_message(self):
+        # self.assertEqual('\nNick Hofmann: 95%', test_score_input.score_input('Nick Hofmann', 95, invalid_message='WRONG '))
